fbad33689209af0a25877a74978ff156ee08cc79
LukeGu <luke.gu.work@gmail.com>
2024-07-16 10:10:24 +1000

    feat: update folder name

--- app/(app)/add-bank-account.tsx
+++ app/(app)/add-bank-account.tsx
@@ -1,22 +1,47 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Calendar as CalendarView } from 'components';
+import { useStyles, TColors } from 'core/theme';
 
 export default function AddBankAccount() {
+  const { styles } = useStyles(createStyles);
   return (
     <View style={styles.container}>
-      <Text>Add bank</Text>
+      <View style={styles.logoContainer}></View>
+      <View>
+        <Text>Basic information</Text>
+        <View style={styles.basicContainer}></View>
+      </View>
+      <View>
+        <Text>Other settings</Text>
+        <View style={styles.moreContainer}></View>
+      </View>
       <StatusBar style='light' />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    gap: 6,
-    padding: 5,
-    backgroundColor: '#fff',
-  },
-});
+const createStyles = (theme: TColors) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.white,
+      gap: 6,
+      padding: 5,
+    },
+    logoContainer: {
+      height: 100,
+      borderRadius: 10,
+      backgroundColor: 'skyblue',
+    },
+    basicContainer: {
+      height: 200,
+      borderRadius: 10,
+      backgroundColor: 'powderblue',
+    },
+    moreContainer: {
+      flex: 1,
+      borderRadius: 10,
+      backgroundColor: 'green',
+    },
+  });
