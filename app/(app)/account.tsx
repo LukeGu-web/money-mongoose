62440d400a83f1ae0927c54b13f54441c3d916b7
LukeGu <luke.gu.work@gmail.com>
2024-07-24 16:09:12 +1000

    feat: add demo user for test

--- app/(app)/account.tsx
+++ app/(app)/account.tsx
@@ -5,12 +5,18 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function Account() {
   const reset = useLocalStore((state) => state.reset);
+  const setToken = useLocalStore((state) => state.setToken);
   const resetRecords = useRecordStore((state) => state.resetRecords);
   const resetAccounts = useAssetStore((state) => state.resetAccounts);
   return (
     <View style={styles.container}>
-      <Button onPress={getAllKeys} title='Get all keys' />
+      {/* <Button onPress={getAllKeys} title='Get all keys' /> */}
+
       <Button onPress={clearAll} title='Clear all keys' />
+      <Button
+        onPress={() => setToken('dd62303261c21c234bc49119a9f8ef613684e64e')}
+        title='Use demo user'
+      />
       <Button onPress={reset} title='Reset local store' />
       <Button onPress={resetRecords} title='Reset records store' />
       <Button onPress={resetAccounts} title='Reset accounts store' />
