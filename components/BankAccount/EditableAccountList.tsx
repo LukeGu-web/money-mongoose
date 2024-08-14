2901f62a86fe8bfe60c443a2b5b476edd4837874
LukeGu <luke.gu.work@gmail.com>
2024-08-14 13:56:31 +1000

    fix: update list styles

--- components/BankAccount/EditableAccountList.tsx
+++ components/BankAccount/EditableAccountList.tsx
@@ -1,5 +1,5 @@
 import { useRef, useCallback } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { FlashList } from '@shopify/flash-list';
 import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import ListItem from './ListItem';
@@ -9,11 +9,7 @@ import { useAsset, useBookStore } from 'core/stateHooks';
 
 export default function EditableAccountList() {
   const asset = useAsset((state) => state.asset);
-  console.log('asset: ', asset);
-
-  let numOfAssets = 0;
   const currentBook = useBookStore((state) => state.currentBook);
-
   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
 
   const handleCloseSheet = useCallback(() => {
@@ -26,41 +22,34 @@ export default function EditableAccountList() {
   const handlePressItem = () => {
     bottomSheetModalRef.current?.present();
   };
+
   return (
     <View className='flex-1 gap-2'>
       {currentBook?.groups.map((group) => {
-        if (group.assets.length > 0) {
-          const assets = group.assets;
-          const title = {
-            text: group.name,
-            number: assets.length,
-            amount: assets.reduce((sum, item) => sum + Number(item.balance), 0),
-          };
-          numOfAssets += assets.length;
-          return (
-            <EditableGroupTitle
-              key={group.id}
-              title={title}
-              height={40 * assets.length}
-            >
-              <View className='flex-1 w-full'>
-                <FlashList
-                  data={assets}
-                  renderItem={({ item }) => (
-                    <ListItem item={item} onPress={handlePressItem} />
-                  )}
-                  estimatedItemSize={10}
-                />
-              </View>
-            </EditableGroupTitle>
-          );
-        }
+        const assets = group.assets;
+        const title = {
+          text: group.name,
+          number: assets.length,
+          amount: assets.reduce((sum, item) => sum + Number(item.balance), 0),
+        };
+        return (
+          <EditableGroupTitle
+            key={group.id}
+            title={title}
+            height={40 * assets.length}
+          >
+            <View className='flex-1 w-full'>
+              <FlashList
+                data={assets}
+                renderItem={({ item }) => (
+                  <ListItem item={item} onPress={handlePressItem} />
+                )}
+                estimatedItemSize={10}
+              />
+            </View>
+          </EditableGroupTitle>
+        );
       })}
-      {numOfAssets === 0 && (
-        <View className='items-center py-4 bg-gray-200 rounded-md'>
-          <Text>No account yet</Text>
-        </View>
-      )}
       <EditAssetGroupBottomSheet
         bottomSheetModalRef={bottomSheetModalRef}
         funtions={functions}
--- components/BottomSheet/EditAssetGroupBottomSheet.tsx
+++ components/BottomSheet/EditAssetGroupBottomSheet.tsx
@@ -24,26 +24,24 @@ export default function EditAssetGroupBottomSheet({
     >
       <View className='items-center justify-between w-full gap-4 px-4'>
         <Text className='text-2xl font-bold'>{title}</Text>
-        <View className='w-full bg-blue-400 rounded-md'>
+        <View className='w-full'>
           {Object.keys(funtions).map((item, index) => (
             <TouchableOpacity
               key={item}
-              className='py-4 border-white'
-              style={{
-                borderBottomWidth:
-                  index !== Object.keys(funtions).length - 1 ? 1 : 0,
-              }}
+              className={`py-3 border-white border-2 rounded-md ${
+                item === 'Delete' ? 'bg-red-500' : 'bg-blue-400'
+              }`}
               onPress={funtions[item]}
             >
-              <Text className='text-center color-white'>{item}</Text>
+              <Text className='text-lg text-center color-white'>{item}</Text>
             </TouchableOpacity>
           ))}
         </View>
         <TouchableOpacity
-          className='w-full py-4 mt-1 border-2 border-gray-500 rounded-md'
+          className='w-full py-3 mt-1 border-2 border-gray-500 rounded-md'
           onPress={onCancel}
         >
-          <Text className='text-center'>Cancel</Text>
+          <Text className='text-lg text-center'>Cancel</Text>
         </TouchableOpacity>
       </View>
     </BottomSheet>
