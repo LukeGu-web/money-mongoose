9e19664bb34755e20bad72fec7143ae026f59ba9
LukeGu <luke.gu.work@gmail.com>
2024-09-15 10:12:21 +1000

    chore: update line pointer

--- components/Chart/Line.tsx
+++ components/Chart/Line.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, Pressable } from 'react-native';
 import { LineChart } from 'react-native-gifted-charts';
 
 type LinePointType = {
@@ -7,34 +7,14 @@ type LinePointType = {
   date: string;
 };
 
-const Pointer = (items: LinePointType[]) => (
-  <View
-    style={{
-      height: 90,
-      width: 100,
-      justifyContent: 'center',
-    }}
-  >
-    <Text
-      style={{
-        color: 'white',
-        fontSize: 14,
-        marginBottom: 6,
-        textAlign: 'center',
-      }}
-    >
-      {items[0].date}
-    </Text>
+const types = ['expense', 'income', 'balance'];
 
-    <View
-      style={{
-        paddingHorizontal: 14,
-        paddingVertical: 6,
-        borderRadius: 16,
-        backgroundColor: 'white',
-      }}
-    >
-      <Text style={{ fontWeight: 'bold', textAlign: 'center' }}>
+const Pointer = (items: LinePointType[]) => (
+  <View className='justify-center w-24 h-20'>
+    <Text className='text-center'>{items[0].date}</Text>
+
+    <View className='px-3 py-1 rounded-2xl bg-slate-200'>
+      <Text className='font-bold text-center'>
         {'$' + items[0].value + '.0'}
       </Text>
     </View>
@@ -49,8 +29,23 @@ type LineProps = {
 };
 
 export default function Line({ data }: LineProps) {
+  const [type, setType] = useState('expense');
+  const [isRight, setIsRight] = useState(false);
+  const handleLabelPosition = ({
+    pointerX,
+  }: {
+    pointerIndex: number;
+    pointerX: number;
+    pointerY: number;
+  }) => {
+    if (pointerX > 200) {
+      setIsRight(true);
+    } else {
+      setIsRight(false);
+    }
+  };
   return (
-    <View className='flex-1 w-full p-2 bg-black'>
+    <View className='flex-1 w-full p-2 bg-white'>
       <LineChart
         areaChart
         data={data}
@@ -62,8 +57,8 @@ export default function Line({ data }: LineProps) {
         thickness={2}
         startFillColor='rgba(20,105,81,0.3)'
         endFillColor='rgba(20,85,81,0.01)'
-        startOpacity={0.9}
-        endOpacity={0.2}
+        startOpacity={0.8}
+        endOpacity={0.1}
         initialSpacing={10}
         noOfSections={3}
         stepHeight={100}
@@ -73,21 +68,41 @@ export default function Line({ data }: LineProps) {
         rulesColor='gray'
         yAxisTextStyle={{ color: 'gray' }}
         xAxisColor='lightgray'
+        getPointerProps={handleLabelPosition}
         pointerConfig={{
-          pointerStripHeight: 160,
+          pointerStripHeight: 200,
           pointerStripColor: 'lightgray',
           pointerStripWidth: 2,
+          shiftPointerLabelX: isRight ? -70 : 10,
           pointerColor: 'lightgray',
           radius: 6,
           pointerLabelWidth: 100,
           pointerLabelHeight: 90,
-          // activatePointersOnLongPress: true,
           autoAdjustPointerLabelPosition: false,
           pointerLabelComponent: Pointer,
         }}
-        // isAnimated
-        // animationDuration={10000}
       />
+      <View className='items-center justify-center'>
+        <View className='flex-row items-center border-2 rounded-lg border-slate-800'>
+          {Object.values(types).map((item, index) => (
+            <Pressable
+              key={item}
+              className={`items-center justify-center py-1 px-2 border-slate-800 ${
+                index < 2 && 'border-r-2'
+              } ${type === item && 'bg-slate-800'}`}
+              onPress={() => setType(item)}
+            >
+              <Text
+                className={`text-center font-medium ${
+                  type === item ? 'color-white' : 'color-slate-800'
+                }`}
+              >
+                {item}
+              </Text>
+            </Pressable>
+          ))}
+        </View>
+      </View>
     </View>
   );
 }
