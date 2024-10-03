9b77159666881160d2dedcfc69787dbbe7321921
LukeGu <luke.gu.work@gmail.com>
2024-10-03 17:08:12 +1000

    chore: setup basic push notifications

--- components/AccountSections/InformationSection.tsx
+++ components/AccountSections/InformationSection.tsx
@@ -5,7 +5,7 @@ import { useSettingStore } from 'core/stateHooks';
 export default function InformationSection() {
   const theme = useSettingStore((state) => state.theme);
   return (
-    <View className='items-start justify-center flex-1 gap-2'>
+    <View className='items-start justify-center flex-1 gap-2 mb-4'>
       <Text className='color-zinc-600 dark:color-zinc-300'>Infomation</Text>
       <View className='w-full rounded-lg bg-zinc-200 dark:bg-zinc-800'>
         <Pressable className='flex-row items-center gap-2 px-4 py-2 border-b-2 border-white dark:border-black'>
--- components/Providers/Providers.tsx
+++ components/Providers/Providers.tsx
@@ -10,6 +10,7 @@ import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { APIProvider } from 'api/api-provider';
 import { useSettingStore } from 'core/stateHooks';
 import log from 'core/logger';
+import { usePushNotifications } from 'core/features/usePushNotifications';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const appState = useRef(AppState.currentState);
@@ -24,6 +25,11 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 
   const isEnabledAuthRef = useRef(isEnabledAuth);
   const lastAuthTimeRef = useRef(0);
+
+  // const { expoPushToken, notification } = usePushNotifications();
+  // const data = JSON.stringify(notification, undefined, 2);
+
+  // console.log('usePushNotifications: ', data);
 
   // Update the ref whenever isEnabledAuth changes
   useEffect(() => {
