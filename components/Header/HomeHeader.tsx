bb990b3061817212ff673f0deada722c43a9593c
LukeGu <luke.gu.work@gmail.com>
2024-10-21 15:54:05 +1100

    chore: create custom drawer toggle

--- components/Header/HomeHeader.tsx
+++ components/Header/HomeHeader.tsx
@@ -1,22 +1,29 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 import { Link } from 'expo-router';
 import { DrawerToggleButton } from '@react-navigation/drawer';
+import {
+  DrawerActions,
+  ParamListBase,
+  useNavigation,
+} from '@react-navigation/native';
 import { useBookStore } from 'core/stateHooks';
 import { Entypo } from '@expo/vector-icons';
 import Icon from '../Icon/Icon';
 
 export default function HomeHeader() {
+  const navigation = useNavigation();
   const currentBook = useBookStore((state) => state.currentBook);
   return (
     <View className='flex-row items-center justify-between h-12 px-1 pb-1 -mt-1 bg-primary'>
-      <View className='flex-row items-center justify-center gap-2'>
-        <View className='bg-white rounded-full'>
-          <DrawerToggleButton />
-        </View>
+      <Pressable
+        className='flex-row items-center justify-center ml-2'
+        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+      >
         <Text className='text-lg font-bold color-white'>
           {currentBook.name}
         </Text>
-      </View>
+        <Icon name='arrow-right' size={24} color='white' />
+      </Pressable>
       <View className='flex-row items-center justify-center gap-4'>
         <Link href='/statistics/trending/' className='pl-3'>
           <Icon name='chart-line' size={24} color='white' />
