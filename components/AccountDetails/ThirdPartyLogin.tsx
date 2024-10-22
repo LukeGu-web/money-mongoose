e76deac79ab6d2da7c706e7b0c46ac249f2a76d6
LukeGu <luke.gu.work@gmail.com>
2024-10-22 17:07:01 +1100

    chore: prebuild

--- components/AccountDetails/ThirdPartyLogin.tsx
+++ components/AccountDetails/ThirdPartyLogin.tsx
@@ -34,6 +34,7 @@ export default function ThirdPartyLogin() {
       // setUserInfo(userInfo);
       // setError(null);
     } catch (e) {
+      console.error('GoogleSignin error: ', e);
       // setError(e);
     }
   };
