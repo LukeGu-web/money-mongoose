1d1176bcf47a09f8e0407a5b032a2d02ed4bedc8
LukeGu <luke.gu.work@gmail.com>
2024-10-22 16:21:14 +1100

    chore: follow online doc

--- components/AccountDetails/ThirdPartyLogin.tsx
+++ components/AccountDetails/ThirdPartyLogin.tsx
@@ -49,20 +49,8 @@ export default function ThirdPartyLogin() {
       <Text className='self-center color-gray-400'>
         ----------- or -----------
       </Text>
-      <View className='gap-3 mt-4'>
-        {/* <Pressable className='flex-row items-center self-center w-3/4 gap-4 px-4 py-2 border-2 border-gray-400 rounded-lg'>
-          <View className='items-center w-1/6'>
-            <FontAwesome6
-              name='google'
-              size={20}
-              color={theme === 'dark' ? '#dbeafe' : '#03045E'}
-            />
-          </View>
-          <Text className='text-lg font-semibold color-primary dark:color-blue-100'>
-            Continue with Google
-          </Text>
-        </Pressable> */}
-        <View className='self-center w-3/4 border-2 border-gray-400 rounded-lg'>
+      <View className='gap-3 mt-6'>
+        <View className='self-center w-3/4 overflow-hidden border-2 border-gray-400 rounded-lg'>
           <GoogleSigninButton
             size={GoogleSigninButton.Size.Standard}
             color={
@@ -71,7 +59,8 @@ export default function ThirdPartyLogin() {
                 : GoogleSigninButton.Color.Light
             }
             style={{
-              width: '100%',
+              width: '103%',
+              margin: -4,
             }}
             onPress={signIn}
           />
