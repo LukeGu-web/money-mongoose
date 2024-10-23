0a103df34a3cfbe6a8a941e3911d5da67e2541ab
LukeGu <luke.gu.work@gmail.com>
2024-10-23 14:57:14 +1100

    chore: install FB login lib

--- app.config.ts
+++ app.config.ts
@@ -11,5 +11,19 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
         iosUrlScheme: process.env.EXPO_PUBLIC_GOOGLE_SIGNIN_IOS_URL,
       },
     ],
+    [
+      'react-native-fbsdk-next',
+      {
+        appID: process.env.EXPO_PUBLIC_FB_SIGNIN_IOS_ID,
+        clientToken: process.env.EXPO_PUBLIC_FB_SIGNIN_IOS_CLIENT_TOKEN,
+        displayName: 'Get Rich',
+        scheme: `fb${process.env.EXPO_PUBLIC_FB_SIGNIN_IOS_ID}`,
+        advertiserIDCollectionEnabled: false,
+        autoLogAppEventsEnabled: false,
+        isAutoInitEnabled: true,
+        iosUserTrackingPermission:
+          'This identifier will be used to deliver personalized ads to you.',
+      },
+    ],
   ],
 });
