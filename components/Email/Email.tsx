2e0f1fbee828dd8b2d6917725d86aa49751d7288
LukeGu <luke.gu.work@gmail.com>
2024-11-05 16:16:11 +1100

    chore: copy to Clipboard

--- components/Email/Email.tsx
+++ components/Email/Email.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
 import { View, Text, Pressable } from 'react-native';
+import * as Clipboard from 'expo-clipboard';
 import * as MailComposer from 'expo-mail-composer';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { useSettingStore } from 'core/stateHooks';
 import Icon from '../Icon/Icon';
+
+const email = 'getrich.help.service@gmail.com';
 
 export default function Email() {
   const theme = useSettingStore((state) => state.theme);
@@ -11,15 +14,28 @@ export default function Email() {
   useEffect(() => {
     async function checkAvailability() {
       const isMailAvailable = await MailComposer.isAvailableAsync();
+      console.log('isMailAvailable: ', isMailAvailable);
       setIsAvailable(isMailAvailable);
     }
 
     checkAvailability();
   }, []);
+
+  //   const copyToClipboard = async () => {
+  //     await Clipboard.setStringAsync(email);
+  //   };
+
+  const handleClickEmail = () => {
+    if (isAvailable) {
+    } else {
+      //   copyToClipboard();
+    }
+  };
+
   return (
     <Pressable
       className='flex-row items-center justify-between gap-2 px-4 py-2'
-      disabled={!isAvailable}
+      onPress={handleClickEmail}
     >
       <View className='flex-row items-center justify-center gap-2'>
         <MaterialCommunityIcons
@@ -29,12 +45,10 @@ export default function Email() {
         />
         <Text className='text-lg font-semibold dark:color-white'>Email:</Text>
       </View>
-      <View className='flex-row items-center justify-center'>
-        <Text className='text-lg dark:color-white'>
-          getrich.help.service@gmail.com
-        </Text>
+      <View className='flex-row items-center justify-center gap-2'>
+        <Text className='text-lg dark:color-white'>{email}</Text>
         <Icon
-          name='arrow-right'
+          name={isAvailable ? 'arrow-right' : 'copy'}
           size={16}
           color={theme === 'dark' ? 'white' : 'black'}
         />
